d38947f9dfdbe30a2051118eafb41f881a6ed34d
tristan <heytristaann@gmail.com>
2023-06-27 08:12:39 -0400

    rename

--- data.py
+++ data.py
@@ -6,8 +6,8 @@ from torchvision.transforms import ToTensor
 class Data:
   def __init__(self):
     # Get the training dataset and define the model
-    self.training: datasets.MNIST = datasets.MNIST(root="data", train=True, download=True, transform=ToTensor())
-    self.model: DataLoader = DataLoader(self.training, batch_size=64, shuffle=True)
+    self.mnist: datasets.MNIST = datasets.MNIST(root="data", train=True, download=True, transform=ToTensor())
+    self.model: DataLoader = DataLoader(self.mnist, batch_size=64, shuffle=True)
 
     # Get the device
     self.device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
