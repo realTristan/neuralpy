3e19c87a78582c19b8b64f29b5025fc21b3cf651
Tristan <heytristaann@gmail.com>
2023-06-28 21:49:11 -0400

    cleaned up

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from trainer import Trainer
 from image import Image
 from torchvision import models
 from constants import DEVICE, MNIST_IMAGE_SIZE
-import torch, PIL, matplotlib.pyplot as plt
+import torch, matplotlib.pyplot as plt
+from PIL import Image as PILImage
 
 # Install PyTorch with CUDA 11.1 (Faster)
 # pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
@@ -25,64 +26,64 @@ def train(model, dataset: str, data) -> None:
 
 
 # Test the mnist dataset
-def test_mnist(model, dataset: str, image: str) -> None:
+def test_mnist(model, dataset: str, image_name: str) -> None:
     # Load the model
     model.load_state_dict(torch.load(dataset))
 
     # Open the image and get the prediction
-    image: PIL.Image = PIL.Image.open(image)
+    image = PILImage.open(image_name)
     image_tensor: torch.Tensor = Image.to_tensor(image)
     pred: torch.Tensor = torch.argmax(model(image_tensor))
 
     # Open the image using matplotlib
     plt.title(f"Prediction: {pred.item()}")
-    plt.imshow(image)
+    plt.imshow(image_tensor.squeeze().numpy(), cmap="gray")
     plt.show()
 
 
 # Test the cats and dogs dataset
-def test_cats_dogs(model, dataset: str, image: str) -> None:
+def test_cats_dogs(model, dataset: str, image_name: str) -> None:
     # Load the model
     model.load_state_dict(torch.load(dataset))
 
     # Open the image and get the prediction
-    image: PIL.Image = PIL.Image.open(image)
+    image = PILImage.open(image_name)
     image_tensor: torch.Tensor = Image.to_tensor(image)
     pred: torch.Tensor = torch.argmax(model(image_tensor))
 
     # Open the image using matplotlib
     plt.title(f"Prediction: {'Cat' if pred.item() == 0 else 'Dog'}")
-    plt.imshow(image)
+    plt.imshow(image_tensor.squeeze().numpy(), cmap="gray")
     plt.show()
 
 
 # Test the drunk and sober dataset
-def test_drunk_sober(model, dataset: str, image: str) -> None:
+def test_drunk_sober(model, dataset: str, image_name: str) -> None:
     # Load the model
     model.load_state_dict(torch.load(dataset))
 
     # Open the image and get the prediction
-    image: PIL.Image = PIL.Image.open(image)
+    image = PILImage.open(image_name)
     image_tensor: torch.Tensor = Image.to_tensor(image)
     pred: torch.Tensor = torch.argmax(model(image_tensor))
 
     # Open the image using matplotlib
     plt.title(f"Prediction: {'Sober' if pred.item() == 0 else 'Drunk'}")
-    plt.imshow(image)
+    plt.imshow(image_tensor.squeeze().numpy(), cmap="gray")
     plt.show()
 
-def test_healthy_unhealthy(model, dataset: str, image: str) -> None:
+def test_healthy_unhealthy(model, dataset: str, image_name: str) -> None:
     # Load the model
     model.load_state_dict(torch.load(dataset))
 
     # Open the image and get the prediction
-    image: PIL.Image = PIL.Image.open(image)
+    image = PILImage.open(image_name)
     image_tensor: torch.Tensor = Image.to_tensor(image)
     pred: torch.Tensor = torch.argmax(model(image_tensor))
 
     # Open the image using matplotlib
     plt.title(f"Prediction: {'Healthy' if pred.item() == 1 else 'Unhealthy'}")
-    plt.imshow(image)
+    plt.imshow(image_tensor.squeeze().numpy(), cmap="gray")
     plt.show()
 
 
--- testing/text_model/constants.py
+++ testing/text_model/constants.py
@@ -3,4 +3,4 @@ import torch
 
 INPUT_SIZE: int = 204
 DEVICE: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-EPOCHS: int = 100
+EPOCHS: int = 1000
