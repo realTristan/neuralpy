cf426b0f1e29e24a729a781275ae4c8ff5f390c2
Tristan <heytristaann@gmail.com>
2023-07-01 07:51:51 -0400

    tmp

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ import torch
 
 
 CHANNELS: int = 3
-EPOCHS: int = 10
+EPOCHS: int = 100
 DEVICE: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MNIST_IMAGE_SIZE: int = 28
